5bef11a3d39f41e1be81c8529296a688ed6cc20a
panbaoshan <0987654321>
2023-08-07 04:29:08 +0800

    Vue3更新数据--jie  8/7

---
+++ ref数据更新响应/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>姓名：{{name}}</div>
+  <div>年龄：{{age}}</div>
+  <button @click="handleValue">点我改变数据</button>
+</template>
+
+<script>
+import {ref} from 'vue'
+
+export default {
+  setup() {
+    let name = ref('路飞')
+    let age = ref('18')
+
+   function handleValue() {
+      name.value = '索隆'
+      age.value = '20'
+    }
+   
+    return {
+      name,
+      age,
+      handleValue
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ 初识Vue3_setup/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>姓名：{{name}}</div>
+  <div>年龄：{{age}}</div>
+  <div @click="sayHellow">打招呼</div>
+</template>
+
+<script>
+export default {
+  setup() {
+    let name = '路飞'
+    let age = '18'
+
+    function sayHellow () {
+      alert(`您好，我叫${name},今年${age}岁`)
+    }
+
+    return {
+      name,
+      age,
+      sayHellow
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
